af23f672980599ae0cd0a9439515b4c1f463c010
Jaimin Kharadi <learnwithjaimin@gmail.com>
2024-10-28 00:02:20 +0530

    initial coomit

---
+++ iife.js
@@ -0,0 +1,9 @@
+(function (message) {
+  const superhero = " Batman";
+  console.log(message + superhero);
+})("I am");
+
+(function (message) {
+  const superhero = "Superman";
+  console.log(superhero + message);
+})("Almighty");
---
+++ index.js
@@ -0,0 +1,4 @@
+const add = require("./add.js");
+require("./iife.js");
+
+// console.log("Hello from Index js", add(1, 2));
