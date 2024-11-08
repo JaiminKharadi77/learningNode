f8c7bf3225d1ec0ac1d125303b85b22ed7678b4e
Jaimin Kharadi <learnwithjaimin@gmail.com>
2024-11-08 23:46:30 +0530

    Node.js Tutorial - 24 - Streams and Buffers

--- index.js
+++ index.js
@@ -1,33 +1,7 @@
-// const EventEmitter = require("node:events");
+const buffer = new Buffer.from("Jaimin");
 
-// const emitter = new EventEmitter();
+buffer.write("EricEagle")
 
-// emitter.on("order-pizza", () => console.log("Pizza is being baked"));
-
-// emitter.emit("order-pizza");
-
-// // passing the argument
-
-// emitter.on("order-pizzaArg", (size, topping) =>
-//   console.log(`${size} Pizza is being baked with ${topping} topping`)
-// );
-
-// emitter.on("order-pizzaArg", (size) => {
-//   if (size === "Large") console.log("Serving Drink Complimentry");
-// });
-
-// emitter.emit("order-pizzaArg", "Large", "Mushroom");
-
-// // will run both functions of event listener order-pizzaArg
-
-const PizzaShop = require("./pizza-shop");
-
-const pizzaShop = new PizzaShop();
-
-pizzaShop.on("order", (size, topping) => {
-  console.log(`${size} Pizza is being baked with ${topping} topping`);
-});
-
-pizzaShop.order("Large", "Mushroom");
-
-pizzaShop.displayOrderNumber();
+console.log(buffer.toJSON());
+console.log(buffer);
+console.log(buffer.toString());
