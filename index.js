2d0d454288d6d66af9a7143a0d19d0b9d59b4419
Jaimin Kharadi <learnwithjaimin@gmail.com>
2024-11-08 19:44:31 +0530

    Node.js Tutorial - 20 - Callback Pattern

--- index.js
+++ index.js
@@ -1,34 +1,10 @@
-const path = require("node:path");
+function greet(name) {
+  console.log(`Hello ${name}`);
+}
 
-// const path = require("path"); <= This Will work as fine as well
+function higherOrderFunction(callback) {
+  const name = "Jaimin";
+  callback(name);
+}
 
-// console.log(__filename);
-// console.log(__dirname);
-
-// console.log(path.basename(__filename));
-// console.log(path.basename(__dirname));
-
-// console.log(path.extname(__filename));
-// console.log(path.extname(__dirname));
-
-// console.log(path.parse(__filename));
-// console.log(path.format(path.parse(__filename)));
-
-// console.log(path.isAbsolute(__filename)); // will return boolean value
-
-// console.log(path.isAbsolute("./data.json")); // console.log will return false
-
-// console.log(path.join("folder1", "folder2", "index.html"));
-// console.log(path.join("/folder1", "folder2", "index.html"));
-// console.log(path.join("/folder1", "//folder2", "index.html"));
-// console.log(path.join("/folder1", "//folder2", "../index.html"));
-
-// console.log(path.join(__dirname, "data.json"));
-// console.log(path.isAbsolute(path.join(__dirname, "data.json"))); // console.log will return false
-
-console.log(path.resolve("folder1", "folder2", "index.html"));
-console.log(path.resolve("/folder1", "folder2", "index.html"));
-console.log(path.resolve("/folder1", "//folder2", "index.html"));
-console.log(path.resolve("/folder1", "//folder2", "../index.html"));
-
-console.log(path.resolve(__dirname, "data.json"));
+higherOrderFunction(greet); // passing fn as an argument
