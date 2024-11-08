8aa0007f0b6b87254f30b544f61bc1727399df8f
Jaimin Kharadi <learnwithjaimin@gmail.com>
2024-11-08 20:24:37 +0530

    Node.js Tutorial - 22 - Extending from EventEmitter

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
-const EventEmitter = require("node:events");
+// const EventEmitter = require("node:events");
 
-const emitter = new EventEmitter();
+// const emitter = new EventEmitter();
 
-emitter.on("order-pizza", () => console.log("Pizza is being baked"));
+// emitter.on("order-pizza", () => console.log("Pizza is being baked"));
 
-emitter.emit("order-pizza");
+// emitter.emit("order-pizza");
 
-// passing the argument
+// // passing the argument
 
-emitter.on("order-pizzaArg", (size, topping) =>
-  console.log(`${size} Pizza is being baked with ${topping} topping`)
-);
+// emitter.on("order-pizzaArg", (size, topping) =>
+//   console.log(`${size} Pizza is being baked with ${topping} topping`)
+// );
 
-emitter.on("order-pizzaArg", (size) => {
-  if (size === "Large") console.log("Serving Drink Complimentry");
+// emitter.on("order-pizzaArg", (size) => {
+//   if (size === "Large") console.log("Serving Drink Complimentry");
+// });
+
+// emitter.emit("order-pizzaArg", "Large", "Mushroom");
+
+// // will run both functions of event listener order-pizzaArg
+
+const PizzaShop = require("./pizza-shop");
+
+const pizzaShop = new PizzaShop();
+
+pizzaShop.on("order", (size, topping) => {
+  console.log(`${size} Pizza is being baked with ${topping} topping`);
 });
 
-emitter.emit("order-pizzaArg", "Large", "Mushroom");
+pizzaShop.order("Large", "Mushroom");
 
-// will run both functions of event listener order-pizzaArg
+pizzaShop.displayOrderNumber();
