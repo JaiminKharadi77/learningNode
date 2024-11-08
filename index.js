3e5b6faabbcc0b69a97042955ba744cb66c5ce68
Jaimin Kharadi <learnwithjaimin@gmail.com>
2024-11-08 19:28:05 +0530

    Node.js Tutorial - 19 - Path Module

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
-const data = require("./data.json");
+const path = require("node:path");
 
-console.log(data);
+// const path = require("path"); <= This Will work as fine as well
 
-// In Terminal if you run node --watch {filename} it will rerun automatically after you save the file
+// console.log(__filename);
+// console.log(__dirname);
+
+// console.log(path.basename(__filename));
+// console.log(path.basename(__dirname));
+
+// console.log(path.extname(__filename));
+// console.log(path.extname(__dirname));
+
+// console.log(path.parse(__filename));
+// console.log(path.format(path.parse(__filename)));
+
+// console.log(path.isAbsolute(__filename)); // will return boolean value
+
+// console.log(path.isAbsolute("./data.json")); // console.log will return false
+
+// console.log(path.join("folder1", "folder2", "index.html"));
+// console.log(path.join("/folder1", "folder2", "index.html"));
+// console.log(path.join("/folder1", "//folder2", "index.html"));
+// console.log(path.join("/folder1", "//folder2", "../index.html"));
+
+// console.log(path.join(__dirname, "data.json"));
+// console.log(path.isAbsolute(path.join(__dirname, "data.json"))); // console.log will return false
+
+console.log(path.resolve("folder1", "folder2", "index.html"));
+console.log(path.resolve("/folder1", "folder2", "index.html"));
+console.log(path.resolve("/folder1", "//folder2", "index.html"));
+console.log(path.resolve("/folder1", "//folder2", "../index.html"));
+
+console.log(path.resolve(__dirname, "data.json"));
