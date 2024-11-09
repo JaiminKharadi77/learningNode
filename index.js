41d93213140ad879396abbc0bfe73c9853d6d0b6
Jaimin Kharadi <learnwithjaimin@gmail.com>
2024-11-09 19:39:45 +0530

    Node.js Tutorial - 26 - fs Module

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
-const buffer = new Buffer.from("Jaimin");
+const fs = require("node:fs");
 
-buffer.write("EricEagle")
+const fileContents = fs.readFileSync("./file.txt", "utf-8");
 
-console.log(buffer.toJSON());
-console.log(buffer);
-console.log(buffer.toString());
+// console.log(fileContents.toString());
+console.log(fileContents);
+
+fs.readFile("./file.txt", "utf-8", (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
+});
+
+fs.writeFileSync("./greet.txt", "Hello World!");
+
+fs.writeFile("./greet.txt", "Hello Jaimin", (error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("File Wriiten");
+  }
+});
+
+
+// Below code will add it instead of overting the code
+fs.writeFile("./greet.txt", " Hi Mr. Kharadi",{flag:"a"}, (error) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("File Wriiten");
+    }
+  });
+  
