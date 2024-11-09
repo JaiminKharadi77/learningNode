c93fda37f00d720bc8964e4fe54c51b9e3d8a69e
Jaimin Kharadi <learnwithjaimin@gmail.com>
2024-11-09 23:11:15 +0530

    Node.js Tutorial - 27 - fs Promise Module

--- index.js
+++ index.js
@@ -1,35 +1,54 @@
-const fs = require("node:fs");
+// const fs = require("node:fs");
 
-const fileContents = fs.readFileSync("./file.txt", "utf-8");
+// const fileContents = fs.readFileSync("./file.txt", "utf-8");
 
-// console.log(fileContents.toString());
-console.log(fileContents);
+// // console.log(fileContents.toString());
+// console.log(fileContents);
 
-fs.readFile("./file.txt", "utf-8", (error, data) => {
-  if (error) {
-    console.log(error);
-  } else {
+// fs.readFile("./file.txt", "utf-8", (error, data) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(data);
+//   }
+// });
+
+// fs.writeFileSync("./greet.txt", "Hello World!");
+
+// fs.writeFile("./greet.txt", "Hello Jaimin", (error) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log("File Wriiten");
+//   }
+// });
+
+// // Below code will add it instead of overting the code
+// fs.writeFile("./greet.txt", " Hi Mr. Kharadi",{flag:"a"}, (error) => {
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       console.log("File Wriiten");
+//     }
+//   });
+
+const fs = require("node:fs/promises");
+
+// console.log("First");
+
+// fs.readFile("file.txt", "utf-8")
+//   .then((data) => console.log(data))
+//   .catch((error) => console.log(error));
+
+// console.log("Second");
+
+async function readFile() {
+  try {
+    const data = await fs.readFile("file.txt", "utf-8");
     console.log(data);
-  }
-});
-
-fs.writeFileSync("./greet.txt", "Hello World!");
-
-fs.writeFile("./greet.txt", "Hello Jaimin", (error) => {
-  if (error) {
+  } catch (error) {
     console.log(error);
-  } else {
-    console.log("File Wriiten");
   }
-});
+}
 
-
-// Below code will add it instead of overting the code
-fs.writeFile("./greet.txt", " Hi Mr. Kharadi",{flag:"a"}, (error) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("File Wriiten");
-    }
-  });
-  
+readFile();
