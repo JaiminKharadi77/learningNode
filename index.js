43e0899012fef2d188fd97d7d414dc6f57593787
Jaimin Kharadi <learnwithjaimin@gmail.com>
2024-11-11 11:18:51 +0530

    Node.js Tutorial - 28 - Streams

--- index.js
+++ index.js
@@ -1,54 +1,13 @@
-// const fs = require("node:fs");
+const fs = require("node:fs");
 
-// const fileContents = fs.readFileSync("./file.txt", "utf-8");
+const readableStream = fs.createReadStream("./file.txt", {
+  encoding: "utf-8",
+  highWaterMark:2, // Now we read data at 2bytes by default its 64kb
+});
 
-// // console.log(fileContents.toString());
-// console.log(fileContents);
+const writeableStream = fs.createWriteStream("./file2.txt");
 
-// fs.readFile("./file.txt", "utf-8", (error, data) => {
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log(data);
-//   }
-// });
-
-// fs.writeFileSync("./greet.txt", "Hello World!");
-
-// fs.writeFile("./greet.txt", "Hello Jaimin", (error) => {
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log("File Wriiten");
-//   }
-// });
-
-// // Below code will add it instead of overting the code
-// fs.writeFile("./greet.txt", " Hi Mr. Kharadi",{flag:"a"}, (error) => {
-//     if (error) {
-//       console.log(error);
-//     } else {
-//       console.log("File Wriiten");
-//     }
-//   });
-
-const fs = require("node:fs/promises");
-
-// console.log("First");
-
-// fs.readFile("file.txt", "utf-8")
-//   .then((data) => console.log(data))
-//   .catch((error) => console.log(error));
-
-// console.log("Second");
-
-async function readFile() {
-  try {
-    const data = await fs.readFile("file.txt", "utf-8");
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-readFile();
+readableStream.on("data", (chunk) => {
+  console.log(chunk);
+  writeableStream.write(chunk);
+});
