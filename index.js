29297791538ec2b45351444bd7ee9c7721b0c443
Jaimin Kharadi <learnwithjaimin@gmail.com>
2024-11-13 12:01:43 +0530

    Node.js Tutorial - 35 - HTTP Routing

--- index.js
+++ index.js
@@ -2,18 +2,27 @@ const http = require("node:http");
 const fs = require("node:fs");
 
 const server = http.createServer((req, res) => {
-  const superhero = {
-    firstName: "Bruce",
-    lastName: "Wayne",
-  };
+  // res.end(req.url);
+  const url = req.url;
 
-  res.writeHead(200, { "Content-type": "text/html" });
-
-  const name = "Jaimin";
-  // fs.createReadStream(__dirname + "/index.html").pipe(res);
-  let html = fs.readFileSync("./index.html", "utf-8");
-  html = html.replace("{{name}}", name);
-  res.end(html);
+  if (url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Home Page");
+  } else if (url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("About Page");
+  } else if (url === "/api") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        firstName: "Bruce",
+        lastName: "Wayne",
+      })
+    );
+  } else {
+    res.writeHead(404);
+    res.end("Page not found");
+  }
 });
 
 server.listen(3000, () => {
