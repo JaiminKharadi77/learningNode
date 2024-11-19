1579a87d6f46f58d8c1659aed32a2704dae1a82b
Jaimin Kharadi <learnwithjaimin@gmail.com>
2024-11-19 23:07:37 +0530

    Node.js Tutorial - 45 - I/O Queue

--- index.js
+++ index.js
@@ -1,36 +1,32 @@
-// setTimeout(() => console.log("this is setTimeout 1"), 0);
-// setTimeout(() => console.log("this is setTimeout 2"), 0);
-// setTimeout(() => console.log("this is setTimeout 3"), 0);
+const fs = require("fs");
 
-// setTimeout(() => console.log("this is setTimeout 1"), 0);
-// setTimeout(() => {
-//   console.log("this is setTimeout 2");
-//   process.nextTick(
-//     console.log.bind(console, "this is the inner next tick inside setTimeout")
-//   );
-// }, 0);
-// setTimeout(() => console.log("this is setTimeout 3"), 0);
-
-
-setTimeout(() => console.log("this is setTimeout 1"), 1000);
-setTimeout(() => console.log("this is setTimeout 2"), 0);
-setTimeout(() => console.log("this is setTimeout 3"), 500);
-
+// fs.readFile(__filename, () => {
+//   console.log("this is readFile 1");
+// });
 
 // process.nextTick(() => console.log("this is process.nextTick 1"));
-// process.nextTick(() => {
-//   console.log("this is process.nextTick 2");
-//   process.nextTick(() =>
-//     console.log("this is the inner next tick inside next tick")
-//   );
-// });
-// process.nextTick(() => console.log("this is process.nextTick 3"));
-
 // Promise.resolve().then(() => console.log("this is Promise.resolve 1"));
-// Promise.resolve().then(() => {
-//   console.log("this is Promise.resolve 2");
-//   process.nextTick(() =>
-//     console.log("this is the inner next tick inside Promise then block")
-//   );
+
+
+/** Experiment 7 - Timer anamoly. Order of execution can never be guaranteed */
+
+// setTimeout(() => console.log("this is setTimeout 1"), 0);
+
+// fs.readFile(__filename, () => {
+//   console.log("this is readFile 1");
 // });
-// Promise.resolve().then(() => console.log("this is Promise.resolve 3"));
+
+
+
+/** Experiment 8 - I/O queue callbacks are executed after Microtask queues callbacks and Timer queue callbacks are executed */
+
+
+fs.readFile(__filename, () => {
+  console.log("this is readFile 1");
+});
+
+process.nextTick(() => console.log("this is process.nextTick 1"));
+Promise.resolve().then(() => console.log("this is Promise.resolve 1"));
+setTimeout(() => console.log("this is setTimeout 1"), 0);
+
+for (let i = 0; i < 1000000000; i++) {}
