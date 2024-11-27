ce885ab130faf72b16a26518c9b189317e89c9f0
Jaimin Kharadi <learnwithjaimin@gmail.com>
2024-11-27 17:58:51 +0530

    Node.js Tutorial - 56 - npm Scripts

--- my-custom-package/index.js
+++ my-custom-package/index.js
@@ -4,7 +4,6 @@ function greet(name) {
   console.log(upperCase(`Hello ${name}, welcome to Codevolution`));
 }
 
-export { greet };
 
-// npm install -g nodemon installing global pacakages
-// npm uninstall -g nodemon uninstalling global packages
+
+export { greet };
