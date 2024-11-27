5bc45a834a2ddd9686f5761e4fce402e65c8a272
Jaimin Kharadi <learnwithjaimin@gmail.com>
2024-11-27 19:08:17 +0530

    Node.js Tutorial - 57 - Publishing an npm Package

--- my-custom-package/index.js
+++ my-custom-package/index.js
@@ -4,6 +4,8 @@ function greet(name) {
   console.log(upperCase(`Hello ${name}, welcome to Codevolution`));
 }
 
-
-
 export { greet };
+
+// npm adduser <username> // sign with proper details
+
+// npm publish // to publish the package
